16b09007a57dc3070083b70f806a26a19c779f5e
Joaquin Grez <joaquingrez@gmail.com>
2017-06-19 20:21:20 -0300

    Form added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def x
+  	@user = User.all
+  end
+
+  def save_user
+  	if params[:name].present? && params[:email].present? && params[:age].present?
+  		@user = User.new(name: params[:name], email: params[:email], age: params[:age])
+  		@user.save
+  		redirect_to pages_x_path, notice: "Usuario creado correctamente"
+  	else
+  		redirect_to pages_x_path, alert: "Ingrese todos los campos solicitados"
+  	end
   end
 end
